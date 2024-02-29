bc44ee5d65d9d030ff2b2988db214bc78cef5358
ShaikSaifuddin7243 <shaiksaif1601@gmail.com>
2024-02-29 14:04:55 +0530

    Dice project

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,66 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          title: Center(child: Text('Dicee')),
           backgroundColor: Colors.red,
-          title: const Text(
-            "Dicee",
-          ),
-          centerTitle: true,
         ),
-        body: const DiceePage(),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNo = 1;
+  int rightDiceNo = 1;
+
+  void changeDiceFace(){
+    setState(() {
+      leftDiceNo = Random().nextInt(6)+1;
+      rightDiceNo = Random().nextInt(6)+1;
+    });
+
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset("images/dice$leftDiceNo.png"),
+
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                setState(() {
+                  rightDiceNo = Random().nextInt(6)+1;
+                });
+              },
+              child: Image.asset("images/dice$rightDiceNo.png"),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
 
-class DiceePage extends StatefulWidget {
-  const DiceePage({super.key});
-
-  @override
-  State<DiceePage> createState() => _DiceePageState();
-}
-
-class _DiceePageState extends State<DiceePage> {
-  int leftDiceNumber = 1, rightDiceNumber = 1;
-  void changeDice(bool side) {
-    setState(() {
-      if (side == true) {
-        leftDiceNumber = Random().nextInt(6) + 1;
-      } else {
-        rightDiceNumber = Random().nextInt(6) + 1;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          child: IconButton(
-            padding: const EdgeInsets.all(16),
-            splashColor: Colors.transparent,
-            splashRadius: 0.1,
-            onPressed: () {
-              changeDice(true);
-            },
-            icon: Image.asset(
-              "images/dice$leftDiceNumber.png",
-            ),
-          ),
-        ),
-        Expanded(
-          child: IconButton(
-            onPressed: () {
-              changeDice(false);
-            },
-            padding: const EdgeInsets.all(16),
-            splashColor: Colors.transparent,
-            splashRadius: 0.1,
-            icon: Image.asset(
-              'images/dice$rightDiceNumber.png',
-            ),
-          ),
-        ),
-      ],
-    );
-  }
-}
